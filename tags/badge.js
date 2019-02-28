5c0af64b3d4c747450d60d2c4bf85ade80c1131e
Dmytri Kleiner <dk@trick.ca>
2019-02-28 17:44:43 +0100

    bernie reduce that redundancy

--- tags/badge.js
+++ tags/badge.js
@@ -267,7 +267,7 @@
           body: upload
         })
 
-        () => {}.then((data) => {
+        (() => {}).then((data) => {
           console.log('uploaded image')
           if (this.refs.postToPage.checked) {
             let query = '?slogan=' + encodeURIComponent(opts.slogan) + '&filename=' + filename
